be35735a27840bd28356e984955eb2435084b950
IHOXOHI <63340434+IHOXOHI@users.noreply.github.com>
2025-01-25 00:31:00 +0100

    Add files via upload

---
+++ go.py
@@ -0,0 +1,24 @@
+from machine import Pin, SPI
+from micropython_rfm9x import *
+from time import sleep_ms
+
+bR = Pin(16, Pin.IN, Pin.PULL_UP)
+led = Pin(25, Pin.OUT)
+
+def check_buttons():
+    if bR.value() == 0:
+        led.on()
+        rfm9x.send(b'Open The Door, Please.')
+        sleep_ms(500)
+        led.off()
+
+RADIO_FREQ_MHZ = 433.0
+CS = Pin(13, Pin.OUT)
+RESET = Pin(7, Pin.OUT)
+spi = SPI(1, baudrate=1_000_000, sck=Pin(14), mosi=Pin(15), miso=Pin(12))
+rfm9x = RFM9x(spi, CS, RESET, RADIO_FREQ_MHZ)
+rfm9x.hight_power = 5
+
+while 1:
+    check_buttons()
+    sleep_ms(10)
---
+++ main.py
@@ -0,0 +1,24 @@
+from machine import Pin, PWM
+from time import sleep_ms, ticks_ms
+
+bL = Pin(17, Pin.IN, Pin.PULL_UP)
+led = PWM(Pin(25))
+led.freq(20)
+
+t1 = ticks_ms()
+t2 = ticks_ms()
+n = 0
+while (t2 - t1) < 10000:
+    if bL.value() == 0:
+        n += 1
+        led.duty_u16(1000)
+        sleep_ms(500)
+        led.duty_u16(0)
+    t2 = ticks_ms()
+
+if n == 1:
+    import go.py
+else:
+    led.duty_u16(100)
+    while n < 100:
+        sleep_ms(10)
